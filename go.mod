d3fd9bc5e3b4d140a783e967a3fd5e2522fd71e2
Michael Muré <batolettre@gmail.com>
2021-08-16 17:43:21 +0200

    initial import

---
+++ go.mod
@@ -0,0 +1,10 @@
+module api-client
+
+go 1.15
+
+require (
+	github.com/ipfs/go-ipfs-files v0.0.8
+	github.com/ipfs/go-ipfs-http-client v0.1.0
+	github.com/ipfs/interface-go-ipfs-core v0.5.0
+	github.com/spf13/pflag v1.0.5
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	ipfsFiles "github.com/ipfs/go-ipfs-files"
+	httpapi "github.com/ipfs/go-ipfs-http-client"
+	caopts "github.com/ipfs/interface-go-ipfs-core/options"
+	flag "github.com/spf13/pflag"
+)
+
+const infuraAPI = "https://ipfs.infura.io:5001"
+
+func main() {
+	projectId := flag.String("id", "", "your Infura ProjectID")
+	projectSecret := flag.String("secret", "", "your Infura ProjectSecret")
+	api := flag.String("url", infuraAPI, "the API URL")
+	pin := flag.Bool("pin", true, "whether or not to pin the data")
+
+	flag.Parse()
+
+	if *projectId == "" {
+		_, _ = fmt.Fprintln(os.Stderr, "parameter --id is required")
+		os.Exit(-1)
+	}
+	if *projectSecret == "" {
+		_, _ = fmt.Fprintln(os.Stderr, "parameter --secret is required")
+		os.Exit(-1)
+	}
+
+	httpClient := &http.Client{}
+	client, err := httpapi.NewURLApiWithClient(*api, httpClient)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+	client.Headers.Add("Authorization", "Basic "+basicAuth(*projectId, *projectSecret))
+
+	args := flag.Args()
+	if len(args) != 1 {
+		_, _ = fmt.Fprintln(os.Stderr, "file or directory path required as an argument")
+		os.Exit(-1)
+	}
+	path := args[0]
+
+	stat, err := os.Lstat(path)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	// also support directory
+	file, err := ipfsFiles.NewSerialFile(path, false, stat)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	// trap Ctrl+C and call cancel on the context
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	defer func() {
+		signal.Stop(c)
+		cancel()
+	}()
+	go func() {
+		select {
+		case <-c:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	resolved, err := client.Unixfs().Add(ctx, file, caopts.Unixfs.Pin(*pin))
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	fmt.Println(resolved)
+}
+
+func basicAuth(projectId, projectSecret string) string {
+	auth := projectId + ":" + projectSecret
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
